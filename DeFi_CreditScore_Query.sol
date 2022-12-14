3abdc8b37fdfc476d054dc652c5892377490d0ac
Joseph Del Giorgio <106563909+Joseph-DelGiorgio@users.noreply.github.com>
2022-12-14 15:22:28 -0700

    Create DeFi_CreditScore_Query.sol

---
+++ DeFi_CreditScore_Query.sol
@@ -0,0 +1,33 @@
+// Here is an example of how the pseudocode for the algorithm to query the Ethereum blockchain for information about which DeFi products are used by 
+// a given Ethereum address could be implemented in the Solidity programming language:
+
+pragma solidity ^0.8.0;
+
+contract QueryDefiProducts {
+  // Address of the MakerDAO contract
+  address makerDAOContractAddress;
+
+  // Address of the AAVE contract
+  address aaveContractAddress;
+
+  constructor(address _makerDAOContractAddress, address _aaveContractAddress) public {
+    makerDAOContractAddress = _makerDAOContractAddress;
+    aaveContractAddress = _aaveContractAddress;
+  }
+
+  function query(address ethereumAddress) public view returns (bool isUsingMakerDAO, bool isUsingAAVE) {
+    // Query the MakerDAO contract to see if the address has any open CDPs
+    isUsingMakerDAO = MakerDAO(makerDAOContractAddress).openCdp(ethereumAddress) != 0;
+
+    // Query the AAVE contract to see if the address has any open loans or deposits
+    UserAccount storage userAccount = AAVE(aaveContractAddress).getUserAccount(ethereumAddress);
+    isUsingAAVE = (
+      userAccount.principalATokenBalance != 0 ||
+      userAccount.principalBStakedBalance != 0 ||
+      userAccount.borrowBalance != 0
+    );
+
+    // Return the information about which DeFi products the address is using
+    return (isUsingMakerDAO, isUsingAAVE);
+  }
+}
