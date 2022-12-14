42472c2b9166a1d26f8936837f31733296376331
Joseph Del Giorgio <106563909+Joseph-DelGiorgio@users.noreply.github.com>
2022-12-14 15:35:32 -0700

    Create FrontEnd_React.js

---
+++ FrontEnd_React.js
@@ -0,0 +1,72 @@
+// Here is an example of how a front-end application that allows anonymous users to build credit history from previously repaid,
+// overcollateralized loans on the Ethereum blockchain could be implemented in JavaScript, using the React framework:
+
+
+
+import React, { useState } from "react";
+import web3 from "./web3"; // web3.js library
+import QueryDefiProducts from "./QueryDefiProducts"; // Address of the QueryDefiProducts contract
+
+function App() {
+  // State variable to store the Ethereum address of the current user
+  const [ethereumAddress, setEthereumAddress] = useState("");
+
+  // State variable to store the credit history of the current user
+  const [creditHistory, setCreditHistory] = useState({
+    isUsingMakerDAO: false,
+    isUsingAAVE: false,
+    repaidLoans: 0,
+    totalCollateral: 0
+  });
+
+  // State variable to store any error messages
+  const [errorMessage, setErrorMessage] = useState("");
+
+  // Function to handle changes to the Ethereum address input field
+  const handleAddressChange = event => {
+    setEthereumAddress(event.target.value);
+  };
+
+  // Function to handle the "Check Credit History" button click
+  const handleCheckCreditHistory = async () => {
+    // Clear any previous error messages
+    setErrorMessage("");
+
+    // Validate the Ethereum address
+    if (!web3.utils.isAddress(ethereumAddress)) {
+      setErrorMessage("Invalid Ethereum address");
+      return;
+    }
+
+    try {
+      // Query the Ethereum blockchain for information about which DeFi products the address is using
+      const queryDefiProducts = await QueryDefiProducts.methods
+        .query(ethereumAddress)
+        .call();
+
+      // Check if the address is using MakerDAO and AAVE, and if so, query the contracts for more information
+      // about the user's credit history
+      let repaidLoans = 0;
+      let totalCollateral = 0;
+      if (queryDefiProducts.isUsingMakerDAO) {
+        // Query the MakerDAO contract to get the number of repaid loans and the total collateral
+        // of the user's CDPs
+        // (Note: this code is simplified and does not include error handling or handling of contract interactions
+        // that require a transaction to be sent to the blockchain)
+        const cdpManager = await MakerDAO.methods.cdpManager().call();
+        const cdps = await MakerDAO.methods.getCdps(cdpManager, ethereumAddress).call();
+        repaidLoans = cdps.length;
+        totalCollateral = cdps.reduce((total, cdp) => total + cdp.collateral, 0);
+      }
+      if (queryDefiProducts.isUsingAAVE) {
+        // Query the AAVE contract to get the number of repaid loans and the total collateral
+        // of the user's loans
+        // (Note: this code is simplified and does not include error handling or handling of contract interactions
+        // that require a transaction to be sent to the blockchain)
+        const aTokenAddress = await AAVE.methods.aTokens(0).call();
+        const aTokenContract = new web3.eth.Contract(aTokenAbi, aTokenAddress);
+        const aTokenBalance = await aTokenContract.methods
+          .balanceOf(ethereumAddress)
+          .call();
+        repaidLoans = aTokenBalance.div(1e18).toNumber();
+       }
